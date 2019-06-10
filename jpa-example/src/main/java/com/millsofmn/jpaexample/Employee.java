9112c92bdb3bd563d3e36b498e5c801a98713c58
Lyle <millsofmn@gmail.com>
2019-06-10 16:27:58 -0500

    add new project where reading from file and creating sql

---
+++ jpa-example/src/main/java/com/millsofmn/jpaexample/Employee.java
@@ -0,0 +1,12 @@
+package com.millsofmn.jpaexample;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employee {
+}
